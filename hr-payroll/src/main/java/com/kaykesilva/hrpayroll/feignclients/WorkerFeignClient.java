b83052f053f758375ab79844eab0e19fccd790dd
Kayke-S <kayke.allogins@gmail.com>
2024-10-07 18:33:32 -0300

    Ribbon load balacing

--- hr-payroll/src/main/java/com/kaykesilva/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/kaykesilva/hrpayroll/feignclients/WorkerFeignClient.java
@@ -9,10 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.kaykesilva.hrpayroll.entities.Worker;
 
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeignClient {
 	
 	@GetMapping(value = "/{id}")
     ResponseEntity<Worker> findById(@PathVariable Long id);
 
 }
+ 
