470f238f506fb6038c87f2d32802002e3adce0c3
Kayke-S <kayke.allogins@gmail.com>
2024-10-07 19:48:13 -0300

    Project hr-eureka-server created

--- hr-worker/src/main/java/com/kaykesilva/hrworker/repositories/WorkerRepository.java
+++ hr-worker/src/main/java/com/kaykesilva/hrworker/repositories/WorkerRepository.java
@@ -1,9 +1,11 @@
 package com.kaykesilva.hrworker.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.kaykesilva.hrworker.entities.Worker;
 
+@Repository
 public interface WorkerRepository extends JpaRepository<Worker, Long>{
 
 }
