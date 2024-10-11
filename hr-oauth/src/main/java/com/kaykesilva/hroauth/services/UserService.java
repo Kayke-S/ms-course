cc1e1c5cf70b84cab904a05b709095a01c3301c3
Kayke-S <kayke.allogins@gmail.com>
2024-10-11 00:28:58 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/kaykesilva/hroauth/services/UserService.java
@@ -0,0 +1,29 @@
+package com.kaykesilva.hroauth.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kaykesilva.hroauth.entities.User;
+import com.kaykesilva.hroauth.feignClients.UserFeignClient;
+
+@Service
+public class UserService {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+	@Autowired
+	private UserFeignClient userfeignClient;
+	
+	public User findByEmail(String email) {
+		User obj = userfeignClient.findByEmail(email).getBody();
+		
+		if(obj == null) {
+			logger.error("Email not found: " + email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		logger.info("Email found: " + email);
+		return obj;
+	}
+}
